9168d3ba4015113a857eca6c2bbf94245e7f330a
EndeavourCHN <3068364094@qq.com>
2025-03-12 12:17:17 +0800

    Scribble

---
+++ Players.java
@@ -0,0 +1,31 @@
+package Scrabble.Scribble;
+
+public class Players {
+    String name;
+    int score;
+
+    public Players() {
+    }
+
+    public Players(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+}
