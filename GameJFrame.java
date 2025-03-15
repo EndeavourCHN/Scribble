1a684d1b4aed2c786fdcd4a04a7cf58b5198b7d0
EndeavourCHN <3068364094@qq.com>
2025-03-15 17:17:34 +0800

    Game Window Commander

--- GameJFrame.java
+++ GameJFrame.java
@@ -1,34 +1,58 @@
 import javax.swing.*;
 
-public class GameJFrame extends JFrame{
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.security.Key;
+
+public class GameJFrame extends JFrame implements KeyListener {
+
     public GameJFrame(){
+        FrontPage();
+
+        initJFrame();
+
+        initBoard();
+
+        this.setVisible(true);    
+    }
+
+    private void FrontPage(){
+        JLabel gameName = new JLabel("Scribble Game");
+        gameName.setBounds(400, 200, 200, 100);
+        this.getContentPane().add(gameName);
+    }
+
+    private void initJFrame(){
         this.setSize(1024, 576);
         this.setTitle("Scribble Game");
         this.setAlwaysOnTop(true);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        JMenuBar jMenuBar = new JMenuBar();
-
-        JMenu functionJMenu = new JMenu("Function");
-        JMenu aboutJMenu = new JMenu("About");
-
-        JMenuItem replayItem = new JMenuItem("Replay");
-        JMenuItem closeItem = new JMenuItem("Close");
-
-        JMenuItem accountItem = new JMenuItem("About");
-
-        functionJMenu.add(replayItem);
-        functionJMenu.add(closeItem);
-
-        aboutJMenu.add(accountItem);
-
-        jMenuBar.add(functionJMenu);
-        jMenuBar.add(aboutJMenu);
-
-        this.setJMenuBar(jMenuBar);
-
-        this.setVisible(true);
-        
+        this.setLayout(null);
+        this.addKeyListener(this);
     }
+
+    private void initBoard(){
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+    }
+    
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    public boolean isGameOver(){
+        return false;
+    }
+
 }
