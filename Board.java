e7fbfd3e6fcb224c3c773f0ff944197de7f501cc
EndeavourCHN <3068364094@qq.com>
2025-03-15 20:09:56 +0800

    Board Manager

--- Board.java
+++ Board.java
@@ -1,5 +1,13 @@
 package Scrabble.Scribble;
 
-public class Board {
-    
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board extends JPanel {
+    public Board() {
+        
+    }
+
 }
